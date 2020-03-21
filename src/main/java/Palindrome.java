f76ecfbd6d0f67295109da073db57422fc0f228b
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-03-20 23:26:33 -0600

    Update Palindrome.java

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,2 +1,31 @@
+import java.io.*;
+import java.util.*;
+
 public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int length = A.length() / 2;
+        boolean dec = false;
+
+        for(int i = 0; i < length; i++)
+        {
+            if(A.charAt(i) == A.charAt(length-i))
+                dec = true;
+
+            else
+                dec = false;
+        }
+
+        if(dec)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+    }
 }
+
+
+
