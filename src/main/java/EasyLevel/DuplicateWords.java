faae9aa72be24c17b0bbb10f603b4582ecb0a60a
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-07 20:24:55 -0600

    Final Solution DuplicateWords.java

--- src/main/java/EasyLevel/DuplicateWords.java
+++ src/main/java/EasyLevel/DuplicateWords.java
@@ -8,8 +8,8 @@ public class DuplicateWords {
 
     public static void main(String[] args) {
 
-        String regex = "/* Write a RegEx matching repeated words here. */";
-        Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
+        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         Scanner in = new Scanner(System.in);
         int numSentences = Integer.parseInt(in.nextLine());
@@ -21,7 +21,7 @@ public class DuplicateWords {
 
             // Check for subsequences of input that match the compiled pattern
             while (m.find()) {
-                input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
+                input = input.replaceAll(m.group(), m.group(1));
             }
 
             // Prints the modified sentence.
