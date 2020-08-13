794b50cb61b88850a6d9ac26abb2d5795fc8f69a
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-08-13 11:47:02 -0600

    Algorith Exersice 1
    Sum of array elements

---
+++ src/main/java/Exercises/TagContentExtractor/Solution.java
@@ -0,0 +1,25 @@
+package Exercises.TagContentExtractor;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+
+           // Pattern pattern = new Pattern("")
+
+            testCases--;
+        }
+    }
+}
+
+
+
