be88f3ee2f7931dd812fe2a7742e8b22ef05e001
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2022-12-14 17:01:43 -0700

    Sorting of collection using the lambda expresion

---
+++ src/main/java/Java8Features/AnonymousClass_LambdaExpressions/SortingOfCollectionUsingLambda.java
@@ -0,0 +1,74 @@
+package Java8Features.AnonymousClass_LambdaExpressions;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingOfCollectionUsingLambda {
+    public static void main(String[] args) {
+
+        List<EmployeeCollection> employeeCollectionList = Arrays.asList(
+                new EmployeeCollection("Sam",1000),
+                new EmployeeCollection("Anj", 2000),
+                new EmployeeCollection("Brij", 3000),
+                new EmployeeCollection("Carry", 500)
+        );
+
+        //METHOD 1: USING COLLECTIONS API && COMPARATOR INTERFACE
+        Comparator<EmployeeCollection> comparatorEmp = new Comparator<EmployeeCollection>() {
+            @Override
+            public int compare(EmployeeCollection e1, EmployeeCollection e2) {
+                return e1.getEmpName().compareTo(e2.getEmpName());
+            }
+        };
+
+        Collections.sort(employeeCollectionList, comparatorEmp);
+
+        //METHOD 2: USING LAMBDA FUNCTION
+        Comparator<EmployeeCollection> comparator = (EmployeeCollection e1, EmployeeCollection e2) -> {
+            return e1.getEmpName().compareTo(e2.getEmpName());
+        };
+
+        Collections.sort(employeeCollectionList, comparator);
+
+
+        System.out.println("Sorted Collection!!!!");
+        System.out.println(employeeCollectionList);
+    }
+}
+
+class EmployeeCollection{
+    private String empName;
+    private int empSalary;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(int empSalary) {
+        this.empSalary = empSalary;
+    }
+
+    public EmployeeCollection(String empName, int empSalary) {
+        this.empName = empName;
+        this.empSalary = empSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeCollection{" +
+                "empName='" + empName + '\'' +
+                ", empSalary=" + empSalary +
+                '}';
+    }
+
+}//EOF CLASS
