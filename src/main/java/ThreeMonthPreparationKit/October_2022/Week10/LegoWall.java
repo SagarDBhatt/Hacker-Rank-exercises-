bfa4e108fd53da8d4f4645010a1821872a7167b6
sagarbhatt.ons1087@gmail.com <sagar.bhatt@dish.com>
2024-03-06 09:50:21 -0700

    Adding the Week 9 mock test

---
+++ src/main/java/ThreeMonthPreparationKit/October_2022/Week10/LegoWall.java
@@ -0,0 +1,21 @@
+/**
+ * The ask is to find the total number of permutation that can be used to make the lego wall. We are given, height and width of the wall and we need to find the total
+ * number of permutation for which we can use the different width lego to build the wall.
+ *
+ * Constraint: No two same dimensional legos can be stacked together.
+ *
+ * we have legos with the dimensions of, height 1 and width 1,2,3,4.
+ *
+ * Sample input:
+ * 2 2
+ *
+ * SO:
+ * 3
+ *
+ * We can make the walls 3 ways.
+ */
+
+package ThreeMonthPreparationKit.October_2022.Week10;
+
+public class LegoWall {
+}
