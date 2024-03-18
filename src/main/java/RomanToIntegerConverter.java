e2614adec94d3b25d4ac55886c2f8c4cb55b51c4
sagarbhatt.ons1087@gmail.com <sagar.bhatt@dish.com>
2024-03-18 11:32:49 -0600

    GHX Round 2 coding interview question

---
+++ src/main/java/RomanToIntegerConverter.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToIntegerConverter {
+    public static void main(String[] args) {
+        System.out.println(SolutionConveter.convert("MDL"));
+        System.out.println(SolutionConveter.convert("IV"));
+        System.out.println(SolutionConveter.convert("MDIV"));
+
+    }//main
+}//class
+
+class SolutionConveter{
+    public static Integer convert(String romanval){
+
+        Integer result;
+        //1. Create the map to map the string with the int values.
+        Map<String, Integer> mapper = new HashMap<String,Integer>();
+        mapper.put("I",1);
+        mapper.put("V",5);
+        mapper.put("X",10);
+        mapper.put("L",50);
+        mapper.put("C",100);
+        mapper.put("D",500);
+        mapper.put("M",1000);
+
+        //2. Validate the values are in asc order.
+        result = isLargetsOrderCalc(romanval,mapper);
+
+        //3. sum each values and return the result.
+
+        //4. subtract the value and return the result.
+        return result;
+    }
+
+    public static Integer isLargetsOrderCalc(String romanVal, Map<String, Integer> mapper){
+        //String[] rom = romanVal.split("");
+        //boolean isMax = true;
+        Integer result = 0;
+
+        for(int i =0; i < romanVal.length()-1; i++){
+            if(mapper.get(String.valueOf(romanVal.charAt(i))) > mapper.get(String.valueOf(romanVal.charAt(i+1)))){
+              result += mapper.get(String.valueOf(romanVal.charAt(i)));
+            }
+            else{
+                result -= mapper.get(String.valueOf(romanVal.charAt(i)));
+            }
+        }
+        result += mapper.get(String.valueOf(romanVal.charAt(romanVal.length()-1)));
+
+        return result;
+    }
+}
